4a63400a08ceb734b34d1945f3f27fa04338e45f
bdew <blackdew@gmail.com>
2022-04-03 23:36:44 +0300

    changed VanillaRegistryManager to use RegistryObject

--- src/main/scala/net/bdew/factorium/worldgen/PlacementModifiers.scala
+++ src/main/scala/net/bdew/factorium/worldgen/PlacementModifiers.scala
@@ -1,16 +1,17 @@
 package net.bdew.factorium.worldgen
 
 import net.bdew.factorium.worldgen.features.{BelowSurfacePlacementConfigType, CountPlacementConfigType, HeightRangePlacementConfigType}
-import net.bdew.lib.managers.{VanillaRegistryManager, VanillaRegistryObject}
+import net.bdew.lib.managers.VanillaRegistryManager
 import net.minecraft.core.Registry
+import net.minecraftforge.registries.RegistryObject
 
 object PlacementModifiers extends VanillaRegistryManager(Registry.PLACEMENT_MODIFIERS) {
-  val countConfig: VanillaRegistryObject[CountPlacementConfigType] =
+  val countConfig: RegistryObject[CountPlacementConfigType] =
     register("count_config", () => new CountPlacementConfigType)
 
-  val heightRangeConfig: VanillaRegistryObject[HeightRangePlacementConfigType] =
+  val heightRangeConfig: RegistryObject[HeightRangePlacementConfigType] =
     register("height_range_config", () => new HeightRangePlacementConfigType)
 
-  val belowSurface: VanillaRegistryObject[BelowSurfacePlacementConfigType] =
+  val belowSurface: RegistryObject[BelowSurfacePlacementConfigType] =
     register("below_surface", () => new BelowSurfacePlacementConfigType)
 }
