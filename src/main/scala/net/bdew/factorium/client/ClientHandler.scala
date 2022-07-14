7ba4dde56fef69de9df00800a4c428179927b874
bdew <blackdew@gmail.com>
2022-07-14 21:59:06 +0300

    set glass render type in model files

--- src/main/scala/net/bdew/factorium/client/ClientHandler.scala
+++ src/main/scala/net/bdew/factorium/client/ClientHandler.scala
@@ -2,32 +2,15 @@ package net.bdew.factorium.client
 
 import net.bdew.factorium.Factorium
 import net.bdew.factorium.registries.Blocks
-import net.minecraft.client.renderer.{ItemBlockRenderTypes, RenderType}
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.client.event.EntityRenderersEvent
 import net.minecraftforge.client.event.ModelEvent.RegisterGeometryLoaders
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
-
-import java.util.function.Predicate
 
 @Mod.EventBusSubscriber(value = Array(Dist.CLIENT), modid = Factorium.ModId, bus = Bus.MOD)
 object ClientHandler {
-  @SubscribeEvent
-  def clientSetup(ev: FMLClientSetupEvent): Unit = {
-    ev.enqueueWork(new Runnable {
-      override def run(): Unit = {
-        val renderTypeGlass: Predicate[RenderType] = (x: RenderType) => x == RenderType.cutout() || x == RenderType.translucent()
-        ItemBlockRenderTypes.setRenderLayer(Blocks.crystalGlass.block.get(), renderTypeGlass)
-        ItemBlockRenderTypes.setRenderLayer(Blocks.reinforcedGlass.block.get(), renderTypeGlass)
-        ItemBlockRenderTypes.setRenderLayer(Blocks.darkGlass.block.get(), renderTypeGlass)
-        ItemBlockRenderTypes.setRenderLayer(Blocks.glowGlass.block.get(), renderTypeGlass)
-      }
-    })
-  }
-
   @SubscribeEvent
   def registerLoaders(ev: RegisterGeometryLoaders): Unit = {
     ev.register("connected_model", CustomModels.connectedModelLoader)
